1ea6a39eddc0d0dbc10651fba289d1f7fc4aa45d
Ramiz khan <ramizkhan@Ramizs-MacBook-Pro-2.local>
2017-10-13 00:03:49 -0500

    first steps

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('Keys are loaded');
+
+var twitterKeys = {
+  consumer_key: 'Q93xFWoZRzsRGE0yxTzJkMeSd',
+  consumer_secret: 'y1ThbPk59GyNqgjiK8rfLkWOAWIdwlcgg88fBga9NbRpX1dGBp',
+  access_token_key: '308216102-05ys1yDByPVl2FJkHeXy4J1OPNrsapETvEIauWdf',
+  access_token_secret: 'BLxzxuchc4dpgLjk4ltt8qtKMcnsvyIafarJ2GwQIsDFB',
+}
+
+module.exports = twitterKeys;
